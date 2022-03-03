5722fdf12fc7a6529ae38daaba5faa333c782e7f
Marco Bertonati <marko.bertonati@gmail.com>
2022-03-03 00:09:49 -0300

    Finished Gatsby Tutorial Part 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,12 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: { name: `blog`, path: `${__dirname}/blog` },
     },
     "gatsby-plugin-mdx",
+
   ],
 };
--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,13 +1,22 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 
 const BlogPost = ({ data }) => {
-  console.log(data);
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -20,6 +29,17 @@ export const query = graphql`
         title
         date(formatString: "MMMM D, YYYY")
         author
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(
+              aspectRatio: 1.78
+              transformOptions: { cropFocus: CENTER }
+            )
+          }
+        }
       }
       body
     }
